e97654f09d3646dca44b8f9f1b5360675019bc30
stephanus yogi <stephanusyogi12@gmail.com>
2022-01-27 09:37:23 +0700

    clear

--- application/helpers/menu_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!function_exists('active_link')){
-    function activate_menu($controller){
-        $CI = get_instance();
-        $class = $CI->router->fetch_class();
-        return ($class == $controller) ? 'nav-active' : '';
-    }
-}
-?>
