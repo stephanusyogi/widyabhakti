0e14b0f29a5b31d9d5fea9338045987b2f24de32
stephanus yogi <stephanusyogi12@gmail.com>
2022-01-27 17:26:08 +0700

    Change API

--- application/views/include/headernomenu.php
+++ application/views/include/headernomenu.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:image" content="<?php echo base_url('assets/img/logo.png') ?>" />
     <!-- Bootstrap -->
     <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.css') ?>" rel="stylesheet">
